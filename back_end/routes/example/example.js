42f81609c99dac72ca2eb82b70b7e8a56436eb3e
José Francisco de Jesús Santos Salazar <josejfss98@gmail.com>
2024-08-25 04:25:55 -0600

    Creación de endpoint signup

--- back_end/routes/example/example.js
+++ back_end/routes/example/example.js
@@ -1,10 +1,7 @@
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-
 module.exports = (express,app) => {
-    
-	app.get('/',upload.any(), async function(req,res){
-        res.status(200).json({response_text:"Hello World"});
-        
-    });
+	app.get('/',function(req,res){
+        res.send('Server Working')
+	})
+
+	
 }
