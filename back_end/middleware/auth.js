c3033e976dd0e6e4bec76da1d04d47e6d4d30c27
José Francisco de Jesús Santos Salazar <josejfss98@gmail.com>
2025-02-10 22:02:05 -0600

    Corrección de la obtención de id_usuario que viene en el JWT

--- back_end/middleware/auth.js
+++ back_end/middleware/auth.js
@@ -7,6 +7,7 @@ const checkAuth = async (req, res, next) => {
         const token = req.headers.authorization.split(' ').pop() //TODO:123123213
         const tokenData = await verifyToken(token)
         if (tokenData.id_usuario) {
+            req.id_usuario = tokenData.id_usuario
             next()
         } else {
             res.status(401)
